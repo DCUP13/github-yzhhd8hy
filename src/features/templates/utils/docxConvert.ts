941077b4c24677e97184664fdbe75430fc47c1e7
DCUP13 <144501544+DCUP13@users.noreply.github.com>
2026-04-22 21:22:58 -0400

    Updated package.json

--- src/features/templates/utils/docxConvert.ts
+++ src/features/templates/utils/docxConvert.ts
@@ -1,4 +1,4 @@
-import JSZip from 'jszip';
+import { asBlob } from 'html-docx-js-typescript';
 
 function blobToBase64(blob: Blob): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -14,54 +14,12 @@ function blobToBase64(blob: Blob): Promise<string> {
 }
 
 function wrapHtmlForDocx(bodyHtml: string): string {
-  return `<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="UTF-8"></head><body>${bodyHtml}</body></html>`;
-}
-
-const CONTENT_TYPES = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
-  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
-  <Default Extension="xml" ContentType="application/xml"/>
-  <Default Extension="htm" ContentType="text/html"/>
-  <Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/>
-</Types>`;
-
-const ROOT_RELS = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/>
-</Relationships>`;
-
-const DOCUMENT_XML = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
-  <w:body>
-    <w:altChunk r:id="htmlChunk" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"/>
-    <w:sectPr>
-      <w:pgSz w:w="12240" w:h="15840"/>
-      <w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/>
-    </w:sectPr>
-  </w:body>
-</w:document>`;
-
-const DOCUMENT_RELS = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-  <Relationship Id="htmlChunk" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/aFChunk" Target="afchunk.htm"/>
-</Relationships>`;
-
-async function buildDocxBlob(html: string): Promise<Blob> {
-  const zip = new JSZip();
-  zip.file('[Content_Types].xml', CONTENT_TYPES);
-  zip.folder('_rels')!.file('.rels', ROOT_RELS);
-  const word = zip.folder('word')!;
-  word.file('document.xml', DOCUMENT_XML);
-  word.folder('_rels')!.file('document.xml.rels', DOCUMENT_RELS);
-  word.file('afchunk.htm', wrapHtmlForDocx(html));
-  return await zip.generateAsync({
-    type: 'blob',
-    mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  });
+  return `<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body>${bodyHtml}</body></html>`;
 }
 
 export async function htmlToDocxJsonContent(html: string): Promise<string> {
-  const blob = await buildDocxBlob(html);
+  const result = await asBlob(wrapHtmlForDocx(html));
+  const blob = result instanceof Blob ? result : new Blob([result as unknown as ArrayBuffer]);
   const base64 = await blobToBase64(blob);
   return JSON.stringify({
     originalFile: base64,
